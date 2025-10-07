f7e12ba004ef31bd105123631d1b5ecbe8fe3033
c21079342 <pollyl@cardiff.ac.uk>
2025-10-07 15:43:54 +0100

    impl basic security

---
+++ src/main/java/com/lp/criticabackend/security/JWTAuth.java
@@ -0,0 +1,57 @@
+package com.lp.criticabackend.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JWTAuth extends OncePerRequestFilter {
+
+    private final SessionUtil sessionUtil;
+
+    public JWTAuth(SessionUtil sessionUtil) {
+        this.sessionUtil = sessionUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        try{
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(new SecretKeySpec(sessionUtil.secretKey.getBytes(), "HmacSHA256"))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            String email = claims.getSubject();
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception e){
+            throw new ServletException(e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
