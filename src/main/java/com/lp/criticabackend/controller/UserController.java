9febd7555572c4be3703b7dbb5e8d7d5d86a63ff
c21079342 <pollyl@cardiff.ac.uk>
2025-11-18 12:22:30 +0000

    search user

--- src/main/java/com/lp/criticabackend/controller/UserController.java
+++ src/main/java/com/lp/criticabackend/controller/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
